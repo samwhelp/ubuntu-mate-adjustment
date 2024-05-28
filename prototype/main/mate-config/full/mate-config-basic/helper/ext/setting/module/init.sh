83fed84a773f0f5d4085bca39891b748f7ff5fb3
samwhelp <samwhelp@users.noreply.github.com>
2024-05-28 11:54:23 +0800

    fix

---
+++ prototype/main/mate-config/full/mate-config-basic/helper/ext/setting/module/init.sh
@@ -0,0 +1,31 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Gsettings
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-gsettings.sh"
+
+
+##
+## Load: Setting / Module / Mate / Config Main
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/mate-config-main.sh"
+
+
+##
+## Load: Setting / Module / Mate / Workspace Main
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/mate-workspace-main.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
