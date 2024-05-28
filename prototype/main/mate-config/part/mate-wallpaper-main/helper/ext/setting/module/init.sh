3c91336dfb3c7fe392a6c8896840299bc396f319
samwhelp <samwhelp@users.noreply.github.com>
2024-05-28 12:53:26 +0800

    fix

---
+++ prototype/main/mate-config/part/mate-wallpaper-main/helper/ext/setting/module/init.sh
@@ -0,0 +1,24 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Gsettings
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-gsettings.sh"
+
+
+##
+## Load: Setting / Module / Mate / Wallpaper Main
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/mate-wallpaper-main.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
