0e7eeb3f847b2193aebac5dc7939068dcf994b6b
samwhelp <samwhelp@users.noreply.github.com>
2024-05-28 13:44:49 +0800

    fix

---
+++ prototype/main/mate-config/part/mate-theme-dark/helper/ext/setting/module/init.sh
@@ -0,0 +1,24 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Gsettings
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-gsettings.sh"
+
+
+##
+## Load: Setting / Module / Mate / Theme Main
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/mate-theme-main.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
